fa0b1044f9e8796af0086bd563755c2237b2c930
Bamidele Oke <clemokey@live.com>
2025-06-21 22:47:11 +0200

    Implemented the directions.py in the new web application

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http import HttpResponse
+from .direction import process
 
-# Create your views here.
+# Index view for the webapp
+def index(request):
+    address = "Karl-Adrian-Straße 1, 5020 Salzburg, Austria"
+    instructions = process(address, "instructions")
+    mapview = process(address,)
+    context = {
+        "instructions": instructions,
+        "map": mapview
+    }
+    return render(request, "webapp/index.html", context)
